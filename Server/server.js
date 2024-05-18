b965555c6add3a8db5a5c9d82c7ac4e8f2e4a98e
vamsikilari777 <kilarivamsikvs777@gmail.com>
2024-05-18 16:40:29 +0530

    Changed all dir

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Configure CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://medimonitorx.vercel.app'],
+  origin: ['http://localhost:3000', 'https://testing-livid-pi.vercel.app'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 // config.js
-const baseURL = process.env.REACT_APP_BASE_URL || 'https://medimonitorx-710d.onrender.com'; // Fallback to default if env var is missing
+const baseURL = process.env.REACT_APP_BASE_URL || 'https://medimonitorx-s9r1.onrender.com'; // Fallback to default if env var is missing
 export default baseURL;

ca6e98daf75062723755ae78441c284ed559530c
BoyiniSravanKumar <boyinisravankumar31@gmail.com>
2024-05-21 11:26:04 +0530

    Added a comment on appointment booking modal

--- client/src/modals/AppointmentBookingModal.js
+++ client/src/modals/AppointmentBookingModal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
 import AppointmentBookingForm from "../components/AppointmentBookingForm";
-
+// Functional component for the appointment booking modal
 function AppointmentBookingModal({ showModal, handleCloseModal }) {
   return (
     <Modal show={showModal} onHide={handleCloseModal}>

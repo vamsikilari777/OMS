b2aced9b814d25cca5df04d7a257b3bc70418238
BoyiniSravanKumar <boyinisravankumar31@gmail.com>
2024-06-03 13:03:04 +0530

    Added data

--- client/src/components/ViewReportHistoryTable.js
+++ client/src/components/ViewReportHistoryTable.js
@@ -2,6 +2,28 @@ import React from 'react';
 import '../assets/css/ViewReportHistory.css';
 
 const ViewReportHistory = () => {
+  // Sample static data
+  const data = [
+    {
+      name: 'Ramu',
+      age: 35,
+      gender: 'Male',
+      hospitalId: 12345,
+      doctorId: 67890,
+      diseases: 'Diabetes',
+      reports: 'Link to Reports 1'
+    },
+    {
+      name: 'shruthi',
+      age: 40,
+      gender: 'Female',
+      hospitalId: 54321,
+      doctorId: 98765,
+      diseases: 'Hypertension',
+      reports: 'Link to Reports 2'
+    }
+  ];
+
   return (
     <div className="view-report-history mt-3">
       <div className="view-report-history-table-responsive">
@@ -18,13 +40,17 @@ const ViewReportHistory = () => {
             </tr>
           </thead>
           <tbody>
-            {/* Add rows here */}
-            <tr>
-             
-            </tr>
-            <tr>
-              
-            </tr>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.gender}</td>
+                <td>{item.hospitalId}</td>
+                <td>{item.doctorId}</td>
+                <td>{item.diseases}</td>
+                <td>{item.reports}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
